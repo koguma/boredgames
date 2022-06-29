4fa91a93e8a174e1d17ddcc2faca3ca82dbd74d8
Yu Kogure <52905664+koguma@users.noreply.github.com>
2022-06-29 16:39:08 +0800

    cleaner guard statements imo

--- back-end/main.py
+++ back-end/main.py
@@ -13,15 +13,19 @@ def read_root():
 def create_room(room: Room):
     new_room = sentinel.create(room)
     
-    if not new_room:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Error creating the room")
-    
+    if new_room == 1:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid game-type")
+    elif new_room == 2:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Room ID must be >= 1 character")
+    elif new_room == 3:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Room ID already exists")
     return new_room
 
 @app.get("/rooms/{type}")
 def get_rooms(type: str):
     result = sentinel.list_rooms(type)
-    if result == False:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Error fetching rooms")
-    
+    if result == 1:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid game-type")
+
     return result
+
